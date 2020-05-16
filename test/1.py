2fc970b316fbd540cfe8fded0e4bcfe4fec0eb72
Yupeng Tang <charles.typ.xjtu@gmail.com>
2020-05-16 22:40:38 +0000

    Update full test

--- test/1.py
+++ test/1.py
@@ -11,8 +11,12 @@ right = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K5', 'K7'],
                       'C': ['C0', 'C1', 'C2', 'C3', 'C4', 'C5'],
                       'D': ['D0', 'D1', 'D2', 'D3', 'D4', 'D5']})
 
+right_2 = pd.DataFrame({'key': ['K5', 'K4', 'K3', 'K5', 'K6', 'K9'],
+                      'C': ['CC0', 'CC1', 'CC2', 'CC3', 'CC4', 'CC5'],
+                      'D': ['DD0', 'DD1', 'DD2', 'DD3', 'DD4', 'DD5']})
 left_full = pd.concat([left, left_2]) 
-result, orizer, intrizer, leftsorter, leftcount = pd.pipeline_merge(left_full, right, slices=1, how="pipeline")
+right_full = pd.concat([right, right_2])
+result, orizer, intrizer, sorter, count = pd.pipeline_merge(left_full, right_full, slices=1, how="pipeline")
 print(result)
 
 keys, fac, intfac = pd.build_hash_table(left['key'])
@@ -20,5 +24,7 @@ keys, fac, intfac = pd.build_hash_table(left_2['key'], factorizer=fac, intfactor
 print(left_full)
 print(keys)
 
-result, orizer, intrizer, leftsorter, leftcount = pd.pipeline_merge(left_full, right, slices=1, how="pipeline", factorizer=fac, intfactorizer=intfac, left_factorized_keys=keys)
+result, fac, intfac, leftsorter, leftcount = pd.pipeline_merge(left_full, right,  factorizer=fac, intfactorizer=intfac, slices=2, how="pipeline",left_factorized_keys=keys)
+print(result)
+result, fac, intfac, leftsorter, leftcount = pd.pipeline_merge(left_full, right_2, factorizer=fac, intfactorizer=intfac, leftsorter=leftsorter, leftcount=leftcount, slices=2, how="pipeline",left_factorized_keys=keys)
 print(result)
